5fb422e1460e0f86dbe90edd0b893eada2aa13b0
Peter D'hondt <dhondt.peter@gmail.com>
2023-04-04 15:48:57 +0200

    taak 1.4 - Koers (removed constructor)

--- src/main/java/be/vdab/koers/domain/Renner.java
+++ src/main/java/be/vdab/koers/domain/Renner.java
@@ -6,18 +6,9 @@ import jakarta.persistence.*;
 @Table(name = "renners")
 public class Renner {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String voornaam;
     private String familienaam;
-
-    public Renner(String voornaam, String familienaam) {
-        this.voornaam = voornaam;
-        this.familienaam = familienaam;
-    }
-    protected Renner() {
-
-    }
 
     public long getId() {
         return id;
